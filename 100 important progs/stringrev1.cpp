4598c69e669684b8ca998e6d2664d715e95eda1c
akshatjhabak <46529718+akshatjhabak@users.noreply.github.com>
2020-04-14 23:54:32 +0530

    Add files via upload

---
+++ 100 important progs/stringrev1.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+string str;
+cout<<"enter the string"<<endl;
+getline	(cin,str);
+cout<<"reverse string is "<<endl;
+for(int i=str.length();i>=0;i--)
+cout<<str[i];
+
+}
